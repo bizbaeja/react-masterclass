a54529cf855006f394a2c7c699d811ef58570098
aeong <aeong@aeongui-MacBookPro.local>
2022-12-04 13:07:53 +0900

    interface 설명

--- my-app/src/Circle.tsx
+++ my-app/src/Circle.tsx
@@ -1,18 +1,14 @@
 import styled from "styled-components";
+//interface 란 object shape 을 TS 에 설명해주는 TS의 개념
+//const x = (a:number, b:number) => a+b TS 에게 변수 ab의 타입을 설명해줬었다면,
+//interface NameProps{ attrs: type; } 이런식으로 설명해준다
 
-interface CirecleProps {
+interface CircleProps {
   bgColor: string;
 }
-
-const Container = styled.div<CirecleProps>`
-  width: 200px;
-  height: 200px;
-  background-color: ${(props) => props.bgColor};
-  border-radius: 100px;
-`;
-
-function Circle({ bgColor }: CirecleProps) {
-  return <Container bgColor={bgColor} />;
+const Container = styled.div``;
+function Circle({ bgColor }: CircleProps) {
+  return <Container />;
 }
 
 export default Circle;
