7074f8c91e4df5eb93f2ce2712c1c3707a0e1903
aeong <aeong@aeongui-MacBookPro.local>
2022-12-05 12:58:03 +0900

    circle

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,7 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
+      <Circle borderColor="tomato" bgColor="teal" />
       <Circle bgColor="tomato" text="나 여기있다" />
     </div>
   );
--- my-app/src/Circle.tsx
+++ my-app/src/Circle.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import styled from "styled-components";
 //interface 란 object shape 을 TS 에 설명해주는 TS의 개념
 //const x = (a:number, b:number) => a+b TS 에게 변수 ab의 타입을 설명해줬었다면,
@@ -26,8 +26,8 @@ interface CircleProps {
 //bgcolor 가 CircleProps 임을 선언해준다.
 function Circle({ bgColor, borderColor, text = "기본 값" }: CircleProps) {
   // useState 의 초기값 타입이 될 수 있는 것을  아래와 같이 지정해준다.
-  const [counter, setCounter] = useState<number | string>(0);
-  setCounter("안녕하십니까 하하");
+  // const [counter, setCounter] = useState<number | string>(0);
+  // setCounter("안녕하십니까 하하");
   return (
     <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
       {text}
