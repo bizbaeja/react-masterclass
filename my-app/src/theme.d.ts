8c89864c89f59352984996e30e987361fe477dc4
aeong <aeong@aeongui-MacBookPro.local>
2022-12-06 11:04:48 +0900

    theme

---
+++ my-app/src/theme.d.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const ligthTheme: DefaulTheme = {
+  bgColor: "#111",
+  textColor: "teal",
+  btnColor: "tomato",
+};
+export const darkTheme: DefaulTheme = {
+  bgColor: "#000",
+  textColor: "tomato",
+  btnColor: "teal",
+};
