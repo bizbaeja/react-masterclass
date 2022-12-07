ba2b186d485f510b2ce356bb0203c5653ae2088c
aeong <aeong@aeongui-MacBookPro.local>
2022-12-07 17:39:35 +0900

    error handling

--- my-v6-app/src/Router.tsx
+++ my-v6-app/src/Router.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./screens/About";
 import Home from "./screens/Home";
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-export default Router;
+import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./screens/ErrorComponent";
+//전체 라우터들의 컨테이너
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "", element: <Home />, errorElement: <ErrorComponent /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
+export default router;
---
+++ my-v6-app/src/screens/ErrorComponent.tsx
@@ -0,0 +1,4 @@
+function ErrorComponent() {
+  return <h1>This componenet is crashed</h1>;
+}
+export default ErrorComponent;
