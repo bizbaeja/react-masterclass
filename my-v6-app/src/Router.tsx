eb08fb0400d7d7af5deacf78209c8d5771e420ef
aeong <aeong@aeongui-MacBookPro.local>
2022-12-08 12:24:41 +0900

    useParams

--- my-v6-app/src/Router.tsx
+++ my-v6-app/src/Router.tsx
@@ -1,20 +1,24 @@
 import { createBrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import About from "./screens/About";
 import Home from "./screens/Home";
 import Root from "./Root";
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./screens/ErrorComponent";
+import User from "./screens/users/User";
 //전체 라우터들의 컨테이너
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <NotFound />,
+
     children: [
       { path: "", element: <Home />, errorElement: <ErrorComponent /> },
       { path: "about", element: <About /> },
+      { path: "users/:userId", element: <User /> },
+      { path: "users" },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 export default router;
---
+++ my-v6-app/src/db.ts
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: 1,
+    name: "Ggu",
+  },
+  {
+    id: 2,
+    name: "Aeong",
+  },
+  {
+    id: 2,
+    name: "Sherlock",
+  },
+];
