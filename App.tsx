c6e675d2858f1df6a6208957c1d5c8d54041d0e2
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-12-14 15:28:01 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import Router from "./Router";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Router />
+    </div>
+  );
+};
+export default App;
---
+++ Coins.tsx
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinLinst = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  margin-bottom: 15px;
+  a {
+    display: flex;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    align-items: center;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+  border-radius: 15px;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+const Loader = styled.span`
+  text-align: center;
+`;
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(1, 100));
+      setLoading(false);
+      console.log(coins);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {loading ? (
+        <Loader>"Loading..."</Loader>
+      ) : (
+        <CoinLinst>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                }}
+                state={{ name: coin.name }}
+              >
+                <Img
+                  src={`https://raw.githubusercontent.com/ErikThiart/cryptocurrency-icons/master/16/${coin.name
+                    .toLowerCase()
+                    .split(" ")
+                    .join("-")}.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinLinst>
+      )}
+    </Container>
+  );
+}
+export default Coins;
